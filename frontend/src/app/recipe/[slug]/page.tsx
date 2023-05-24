16d8bd2f8d775d2e2a47a329f0d48a0acc2aaec3
Liam <liam.s.smith@gmail.com>
2023-05-24 19:59:38 -0400

    create dynamic slug path

---
+++ frontend/src/app/recipe/[slug]/page.tsx
@@ -0,0 +1,26 @@
+import client from "../../../../client";
+
+// generate static params function used with dynamic route segments to statically generate routes at build time.
+// replaces useStaticProps in NextJS 13
+
+export async function generateStaticParams() {
+  const paths = await client.fetch(
+    `*[_type == "recipe" && defined(slug.current)][].slug.current`
+  );
+  console.log("Paths", paths);
+  return paths.map((path: any) => ({ slug: path.slug }));
+  // fallback: true,
+}
+
+export default function Page({ params }: any) {
+  // const router = useRouter();
+  const { slug } = params;
+  console.log("Ingredients *********", slug);
+  return (
+    <article>
+      <h1>{slug}</h1>
+      {/* <h1>{name}</h1>
+      <p>{}</p> */}
+    </article>
+  );
+}
