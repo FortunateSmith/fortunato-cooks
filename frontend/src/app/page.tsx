1dfd984d819a38b6b6c1273c743b1dc67a32e97a
Liam <liam.s.smith@gmail.com>
2023-05-30 13:46:45 -0400

    import header component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Header from './components/header'
 
 export default function Home() {
   return (
     <div>
-      <h1>Welcome to Fortunato Cooks!</h1>
+      {/* <h1>Welcome to Fortunato Cooks!</h1> */}
+      <Header/>
     </div>
   )
 }
